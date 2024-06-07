7c5d50b078acd4eb5a287f2880e96d0fef899c64
i1Cps <nohacks2701@gmail.com>
2024-06-07 20:08:34 +0100

    polish ppo

--- algorithms/ppo/utils.py
+++ algorithms/ppo/utils.py
@@ -9,16 +9,4 @@ def plot_learning_curve(x, scores, figure_file):
     plt.plot(x, running_avg)
     plt.title("Running average of previous 100 scores")
     plt.savefig(figure_file)
-
-
-def plot_learning_curve_from_npy(
-    steps_file, scores_file, filename, title="reinforcement learning algorithm returns"
-):
-    steps = np.load(steps_file)
-    scores = np.load(scores_file)
-    running_avg = np.zeros(len(scores))
-    for i in range(len(running_avg)):
-        running_avg[i] = np.mean(scores[max(0, i - 100) : (i + 1)])
-    plt.plot(steps, running_avg)
-    plt.title("MAPPO average returns")
-    plt.savefig(filename)
+    plt.show()
